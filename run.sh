aa3ce15bac9ece291140e17086061125f51eb26a
Sergio Delgado Quintero <sdelquin@gmail.com>
2022-12-06 20:42:04 +0000

    Fix launch of master script

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 cd "$(dirname "$0")"
 source ~/.pyenv/versions/pycheck.es/bin/activate
-gunicorn -b unix:/tmp/pycheck.es.sock main.wsgi:application
+exec gunicorn -b unix:/tmp/pycheck.es.sock main.wsgi:application
