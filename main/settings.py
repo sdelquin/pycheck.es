da53f691371f63d9f2bb052620bcbce9b4810f59
Sergio Delgado Quintero <sdelquin@gmail.com>
2022-12-14 15:44:12 +0000

    Add support for PostgreSQL

--- main/settings.py
+++ main/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
+from prettyconf import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -76,8 +78,10 @@ WSGI_APPLICATION = "main.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config('DB_NAME', default='pycheck'),
+        "USER": config('DB_USER', default='pycheck_user'),
+        "PASSWORD": config('DB_PASSWORD', default='pycheck_pass'),
     }
 }
 
