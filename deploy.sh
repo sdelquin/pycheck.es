9cd4cad8b25ae6655533948587a447c002e8b655
Sergio Delgado Quintero <sdelquin@gmail.com>
2022-12-14 16:41:22 +0000

    Add static collection on deployment

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,5 @@ source ~/.pyenv/versions/pycheck.es/bin/activate
 cd "$(dirname "$0")"
 git pull
 pip install -r requirements.txt
+python manage.py collectstatic --no-input
 supervisorctl restart pycheck.es
