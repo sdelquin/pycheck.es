f4537d2783244d01dd168b2686124f4c3684f68c
Sergio Delgado Quintero <sdelquin@gmail.com>
2022-12-14 16:43:52 +0000

    Add customizable secret key

--- main/settings.py
+++ main/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-aix10ikkq^w-)9f&w@xap8686e#)tvf1%rq73)$lg+si8%dm*_"
+SECRET_KEY = config('SECRET_KEY', default='hello-world')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=config.boolean)
