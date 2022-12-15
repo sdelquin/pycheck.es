1415f86c656f0b83295d93cf83535c3e1bbc1465
Sergio Delgado Quintero <sdelquin@gmail.com>
2022-12-15 16:00:51 +0000

    Fix location of node modules

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ source ~/.pyenv/versions/pycheck.es/bin/activate
 cd "$(dirname "$0")"
 git pull
 pip install -r requirements.txt
-npm install
+npm install --prefix assets/static/assets
 python manage.py collectstatic --no-input
 supervisorctl restart pycheck.es
