4af645b9e04ad49991c7de4e167af1e343460184
Sergio Delgado Quintero <sdelquin@gmail.com>
2022-12-15 09:37:18 +0000

    Add django compressor to static files finders

--- main/settings.py
+++ main/settings.py
@@ -125,6 +125,12 @@ USE_TZ = True
 STATIC_URL = "static/"
 STATIC_ROOT = config('STATIC_ROOT', default='')
 
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'compressor.finders.CompressorFinder',
+)
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
