e5106b4ac7249d659db70f3b6445cbe6244b3750
Sergio Delgado Quintero <sdelquin@gmail.com>
2022-12-20 11:21:37 +0000

    Add content to usage

--- common/static/common/js/base.js
+++ common/static/common/js/base.js
@@ -7,11 +7,41 @@ document.addEventListener(
 );
 
 function documentReady() {
+  setupCodeBlocks();
+  fixContentHeight();
+  setAnchorTop();
+  window.addEventListener("resize", fixContentHeight);
+  document
+    .querySelector("button.navbar-toggler")
+    .addEventListener("click", () => setTimeout(fixContentHeight, 500));
+}
+
+function setupCodeBlocks() {
   document.querySelectorAll("div.code").forEach((el) => {
     el.innerHTML = el.innerHTML.trim();
     hljs.highlightElement(el);
     if (el.classList.contains("linenumbers")) {
       hljs.lineNumbersBlock(el);
     }
+  });
+}
+
+function fixContentHeight() {
+  let header = document.querySelector("header");
+  let footer = document.querySelector("footer");
+  let content = document.querySelector("div.content");
+  const headerHeight = header.offsetHeight;
+  console.log(headerHeight);
+  const footerHeight = footer.offsetHeight;
+  const contentHeight = window.innerHeight - headerHeight - footerHeight;
+  content.style.minHeight = `${contentHeight}px`;
+}
+
+function setAnchorTop() {
+  let header = document.querySelector("header");
+  const headerHeight = header.offsetHeight;
+  const finalGap = headerHeight + 20;
+  document.querySelectorAll("a.anchor").forEach((el) => {
+    el.style.top = `-${finalGap}px`;
   });
 }
--- main/urls.py
+++ main/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("usage/", include('usage.urls')),
+    path("usage/", include(('usage.urls', 'usage'), namespace='usage')),
     path("admin/", admin.site.urls),
 ]
